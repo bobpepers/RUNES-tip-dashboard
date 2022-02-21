0a9edcdc3417057b14f46cc4e65a9333e4ddfa23
Bob Pepers <bob.pepers@gmail.com>
2022-02-21 09:43:27 +0100

    clear insert fields after add

--- src/app/views/TriviaManagement.jsx
+++ src/app/views/TriviaManagement.jsx
@@ -97,7 +97,9 @@ const TriviaManagement = function (props) {
     dispatch(insertTriviaAction({
       question: inputQuestion,
       answers: inputAnswerList,
-    }))
+    }));
+    setInputAnswerList([{ answer: '', correct: 'false' }]);
+    setInputQuestion('');
   };
 
   const handleRemoveClick = (index) => {
