aac8b4f91d4d3ec03447147133270e140588bea7
bobpepers <bob.pepers@gmail.com>
2022-09-11 20:08:57 +0200

    fix: ⚡️ default theme dark, save theme state to localstorage

--- src/app/actions/changeTheme.js
+++ src/app/actions/changeTheme.js
@@ -3,6 +3,7 @@ import {
 } from './types/index';
 
 export function changeTheme(payload) {
+  localStorage.setItem('theme', payload);
   return function (dispatch) {
     dispatch({
       type: THEME_TOGGLE,
--- src/app/reducers/changeTheme.js
+++ src/app/reducers/changeTheme.js
@@ -3,7 +3,7 @@ import {
 } from '../actions/types/index';
 
 const initialState = {
-  theme: 'light',
+  theme: localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark',
 };
 
 export default (state = initialState, action) => {
